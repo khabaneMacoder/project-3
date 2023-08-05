4c48f9cbaba83984515f89016bd7f0bae2309e88
unknown <khabanemohapi9@gmail.com>
2023-08-06 00:03:32 +0200

    first commit

---
+++ Phase3_Project/src/main/java/com/example/Phase3_Project/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.Phase3_Project.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.Phase3_Project.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	//custom query methods
+	List<User> findByUsername(String username);
+	
+	User findByEmail(String email);
+
+}
